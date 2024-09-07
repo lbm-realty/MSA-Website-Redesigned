ed38e8418b39d1ef9dd1f78f41840ec7c72233e5
Labeeb Muntasir <labeebmuntasir@gmail.com>
2024-09-07 15:46:38 -0500

    1.3

--- src/components/faqContainers.jsx
+++ src/components/faqContainers.jsx
@@ -1,50 +1,28 @@
-import '../css/faqContainer.css'
-import question1 from '../images/Question1.png'
-import question2 from '../images/Question2.png'
-import question3 from '../images/Question3.png'
-import question4 from '../images/Question4.png'
-import question5 from '../images/Question5.png'
-import question6 from '../images/Question6.png'
+import "../css/faqContainer.css";
+import question1 from "../images/Question1.png";
+import question2 from "../images/Question2.png";
+import question3 from "../images/Question3.png";
+import question4 from "../images/Question4.png";
+import question5 from "../images/Question5.png";
+import question6 from "../images/Question6.png";
 
 function FaqContainer() {
   return (
     <>
-      <div class="container">
-        <div class="wrapper">
-          <div class="wrapper-holder">
-            <img
-              src={question1}
-              id="slider-img-1"
-              class="images"
-            />
-            <img
-              src={question2}
-              id="slider-img-2"
-              class="images"
-            />
-            <img
-              src={question3}
-              id="slider-img-3"
-              class="images"
-            />
-            <img
-              src={question4}
-              id="slider-img-4"
-              class="images"
-            />
-            <img
-              src={question5}
-              id="slider-img-5"
-              class="images"
-            />
-            <img
-              src={question6}
-              id="slider-img-6"
-              class="images"
-            />
+      <body>
+        <div class="container">
+          <div class="wrapper">
+            <div class="wrapper-holder">
+              <img src={question1} id="slider-img-1" class="images" />
+              <img src={question2} id="slider-img-2" class="images" />
+              <img src={question3} id="slider-img-3" class="images" />
+              <img src={question4} id="slider-img-4" class="images" />
+              <img src={question5} id="slider-img-5" class="images" />
+              <img src={question6} id="slider-img-6" class="images" />
+            </div>
           </div>
         </div>
-      </div>
+      </body>
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,49 @@
+import { useEffect, useState } from 'react';
 import '../css/navbar.css'
 import logo from '../images/msa-logo.png'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 600);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+
+  })
+
+  function handleClick() {
+    const menuList = document.getElementById("menuList");
+    if (menuList.style.height == '0px')
+      menuList.style.height = '300px'
+    else 
+      menuList.style.height = '0px'
+  }
+
+  return (
       <div>
         <div id="navbar" className="navbar">
           <img src={logo} className="logo" /><Link to='/'></Link>
           <ul id="menuList">
             <li className="navbar-text"><Link to='/'>Home</Link></li>
             <li className="navbar-text"><Link to='/faqs'>FAQS</Link></li>
-            <li className="navbar-text"><Link to='/donations'>NEWSLETTER</Link></li>
-            <li className="navbar-text"><Link to='/newsletter'>DONATIONS</Link></li>
+            <li className="navbar-text"><Link to='/newsletter'>NEWSLETTER</Link></li>
+            <li className="navbar-text"><Link to='/donations'>DONATE</Link></li>
             <li className="navbar-text"><Link to='/events'>EVENTS</Link></li>
           </ul>
-          <div id="menu-icon" className="menu-icon">
+          {isMobile && (
+           <div id="menu-icon" onClick={handleClick} className='menu-icon'>
             <i className="fa-solid fa-bars"></i>
-          </div>
+          </div> 
+          )} 
         </div>
       </div>
     );
