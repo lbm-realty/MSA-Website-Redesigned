85052fea4d37948eb2900bd17db1bc1e48e0c508
Labeeb Muntasir <labeebmuntasir@gmail.com>
2024-09-22 11:38:16 -0500

    commit 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Home from "./pages/home";
